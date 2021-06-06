9722da84a20e255af4b9bf7367a40a1e4c568b4b
Richard Ng <richardcrng@gmail.com>
2021-06-06 20:30:24 +0100

    Set up testing skeleton

---
+++ src/skeleton.test.ts
@@ -0,0 +1,5 @@
+import sum from "./skeleton";
+
+test("sum adds two numbers", () => {
+  expect(sum(4, 3)).toBe(7);
+});
---
+++ src/skeleton.ts
@@ -0,0 +1,11 @@
+/**
+ * Adds together two numbers
+ * @param a - the first number to add
+ * @param b - the second number to add
+ * @returns the total
+ */
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+export default sum;
