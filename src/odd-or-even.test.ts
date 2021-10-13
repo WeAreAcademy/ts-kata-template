c728ff0088cb3d5786d08ec38b7f3ee4fb96b901
Jamie Lou <jlou711@googlemail.com>
2021-10-13 13:42:38 +0100

    Add odd or even kata

---
+++ src/odd-or-even.test.ts
@@ -0,0 +1,8 @@
+import oddOrEven from "./odd-or-even";
+
+test("Determine whether sum of the elements is odd or even", () => {
+  expect(oddOrEven([4, 3])).toBe("odd");
+  expect(oddOrEven([])).toBe("even");
+  expect(oddOrEven([1,2, 3])).toBe("even");
+  expect(oddOrEven([-4, 3])).toBe("odd");
+});
---
+++ src/odd-or-even.ts
@@ -0,0 +1,22 @@
+/*
+
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+*/
+
+/**
+ * Given a list of integers, determine whether the sum of its elements is odd or even.
+ * 
+ * @param numArr - input number array to sum
+ * @returns - odd or even depending on sum value.
+ */
+
+function oddOrEven(numArr: number[]): string {
+  return (numArr.reduce((x,y) => (x+y),0) % 2 === 0 ? "even" : "odd")
+}
+
+export default oddOrEven;
