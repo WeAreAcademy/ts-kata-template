7558f54b5b4a7df2cf0b46472a69798190e76c94
Jamie Lou <jlou711@googlemail.com>
2021-10-13 13:44:27 +0100

    Run Prettier and added extra test to oddOrEven

--- src/odd-or-even.ts
+++ src/odd-or-even.ts
@@ -10,13 +10,13 @@ If the input array is empty consider it as: [0] (array with a zero).
 
 /**
  * Given a list of integers, determine whether the sum of its elements is odd or even.
- * 
+ *
  * @param numArr - input number array to sum
  * @returns - odd or even depending on sum value.
  */
 
 function oddOrEven(numArr: number[]): string {
-  return (numArr.reduce((x,y) => (x+y),0) % 2 === 0 ? "even" : "odd")
+  return numArr.reduce((x, y) => x + y, 0) % 2 === 0 ? "even" : "odd";
 }
 
 export default oddOrEven;
