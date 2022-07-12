878d2de64d951ce8f6087bce8f1ce5e2a30a4fa9
Salman H <Salman0112358@outlook.com>
2022-07-12 14:00:25 +0100

    saving before branching

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -2,10 +2,13 @@
 
 /**
  * 
- * @param hexString 
- * @returns 
+ * @param hexString accepts string showing hex value
+ * @returns object containg key value pairs  
  */
-function hexStringToRGB(hexString) {
+function hexStringToRGB(hexString : string) : object  {
+
+  const decimal : string[] =  ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
+  const hex : string[] =["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 
   return solution
 
