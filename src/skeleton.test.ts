afd368bec02b31519d82e2d4ad8d8750b7b74c21
Salman H <Salman0112358@outlook.com>
2022-07-12 14:33:54 +0100

    adding more test

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,8 @@
 import hexStringToRGB from "./skeleton";
 
-test("sum adds two numbers", () => {
-  expect(hexStringToRGB("#FF9933")).toBe({ r: 255, g: 153, b: 51 });
+test("converts a hex string to RGB key value pairs", () => {
+  expect(hexStringToRGB("#FF9933")).toEqual({ r: 255, g: 153, b: 51 });
+  expect(hexStringToRGB("E5FFCC")).toEqual({ r: 229, g: 255, b: 204 });
+  //expect(hexStringToRGB("#FF9933")).toEqual({ r: 255, g: 153, b: 51 });
+  //expect(hexStringToRGB("#FF9933")).toEqual({ r: 255, g: 153, b: 51 });
 });
