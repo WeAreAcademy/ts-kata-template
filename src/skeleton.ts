97b57d9b0a909d07ab53027810ff6cc6444aaa07
Salman H <Salman0112358@outlook.com>
2022-07-12 14:26:18 +0100

    more fixes

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -1,82 +1,55 @@
-/**
- *
- * @param hexString accepts string showing hex value
- * @returns object containg key value pairs
- */
-function hexStringToRGB(hexString: string): object {
-  const decimal: string[] = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-  ];
-  const hex: string[] = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-  ];
 
-  const RGB: number[] = [];
+
+/**
+ * 
+ * @param hexString accepts string showing hex value
+ * @returns  
+ */
+function hexStringToRGB(hexString : string) : typeof solution  {
+
+  const decimal : string[] =  ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
+  const hex : string[] =["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+
+  const RGB : number[] = []
 
   interface solution {
-    r: number;
+    r: number
     g: number;
     b: number;
   }
 
-  let solution: solution;
+  
 
-  hexString = hexString.replace(/#/gi, "").toUpperCase();
 
-  const red: string[] = hexString.substring(0, 2).split("");
-  const green: string[] = hexString.substring(2, 4).split("");
-  const blue: string[] = hexString.substring(4, 6).split("");
-  const colours = [red, green, blue];
+  hexString = hexString.replace(/#/gi, '').toUpperCase()
 
-  colours.forEach((element) => {
-    const firstValue: string = decimal[hex.indexOf(element[0])];
+  const red : string[] = hexString.substring(0,2).split("")
+  const green : string[] = hexString.substring(2,4).split("")
+  const blue : string[] = hexString.substring(4,6).split("")
+  const colours  = [red,green,blue]
+
+  colours.forEach(element =>{
+    
+    let firstValue : string = decimal[hex.indexOf(element[0])]
     //console.log(firstValue)
-    const secondValue: string = decimal[hex.indexOf(element[1])];
+    let secondValue : string = decimal[hex.indexOf(element[1])]
     //console.log(secondValue)
+    
+    let result : number = Number(firstValue)*16 + Number(secondValue)
+  
+    RGB.push(result)
+    
+  })
 
-    const result: number = Number(firstValue * 16) + Number(secondValue);
-
-    RGB.push(result);
-  });
-
-  solution = {
+  const solution : solution = {
     r: RGB[0],
     g: RGB[1],
-    b: RGB[2],
-  };
+    b: RGB[2]
+  }
 
-  console.log(solution);
-  return solution;
+  console.log(solution)
+  return solution
+
 }
 
 export default hexStringToRGB;
