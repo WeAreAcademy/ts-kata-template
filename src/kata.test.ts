73bb51661d36ff93da7d130ad570a8d237dc8bb6
nbogie <neillbogie@googlemail.com>
2022-09-05 12:42:11 +0100

    ts-node-dev and kata ready

---
+++ src/kata.test.ts
@@ -0,0 +1,5 @@
+import { foo } from "./kata";
+
+test("something happens", () => {
+  expect(4 + 3).toBe(7);
+});
---
+++ src/kata.ts
@@ -0,0 +1,7 @@
+console.log("*".repeat(80));
+console.log("hi from kata.ts");
+
+function foo() {
+  return 42;
+}
+export { foo };
