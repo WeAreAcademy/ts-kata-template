40fc7e8b65b1b3ac6d65117776011f6a1e27d33f
nbogie <neillbogie@googlemail.com>
2023-06-23 10:04:02 +0000

    add another test to skeleton test.

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -3,3 +3,8 @@ import sum from "./skeleton";
 test("sum adds two numbers", () => {
   expect(sum(4, 3)).toBe(7);
 });
+
+
+test("sum adds two numbers cancelling", () => {
+  expect(sum(-4, 4)).toBe(0);
+});
