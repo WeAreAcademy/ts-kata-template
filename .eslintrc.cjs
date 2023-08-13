196855317ea874bd3a8aa8050b64e8674251380c
nbogie <neillbogie@googlemail.com>
2023-08-13 10:44:31 +0100

    formatting only

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,19 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended', 
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
-  ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  root: true,
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    root: true,
 };
 
 //More detail about this eslint setup for TypeScript
 //https://typescript-eslint.io/getting-started/
 //https://typescript-eslint.io/linting/typed-linting
-
