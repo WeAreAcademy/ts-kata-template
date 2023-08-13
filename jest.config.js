ad54484884d84c4a0a8d0b39961c045b394e69de
nbogie <neillbogie@googlemail.com>
2023-08-13 11:08:10 +0100

    formatting only

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  //With this ts-jest default preset, 
-  //TypeScript files (.ts, .tsx) will be transformed by ts-jest to CommonJS syntax, 
-  //leaving JavaScript files (.js, jsx) as-is
-  //More about ts-jest presets: 
-  //https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
-  preset: 'ts-jest',
-  
-  //The test environment that will be used for testing. 
-  //The default environment in Jest is a Node.js environment. 
-  //If you are building a web app, you can use a browser-like environment through jsdom instead.
-  //https://jestjs.io/docs/configuration#testenvironment-string
-  testEnvironment: 'node',
+    //With this ts-jest default preset,
+    //TypeScript files (.ts, .tsx) will be transformed by ts-jest to CommonJS syntax,
+    //leaving JavaScript files (.js, jsx) as-is
+    //More about ts-jest presets:
+    //https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
+    preset: "ts-jest",
+
+    //The test environment that will be used for testing.
+    //The default environment in Jest is a Node.js environment.
+    //If you are building a web app, you can use a browser-like environment through jsdom instead.
+    //https://jestjs.io/docs/configuration#testenvironment-string
+    testEnvironment: "node",
 };
